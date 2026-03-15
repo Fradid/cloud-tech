c36f77b2c9d578e5567ec06476226ec58045d922
Fradid <bogd4n55@gmail.com>
2026-03-15 19:43:45 +0200

    upd: terraform format

--- backend.tf
+++ backend.tf
@@ -1,9 +1,9 @@
 terraform {
-	backend "s3" {
-		bucket = "tf-state-lab3-klochko-bohdan-04"
-		key = "env/dev/var-04.tfstate"
-		region = "eu-central-1"
-		encrypt = true
-		use_lockfile = true
-	}
+  backend "s3" {
+    bucket       = "tf-state-lab3-klochko-bohdan-04"
+    key          = "env/dev/var-04.tfstate"
+    region       = "eu-central-1"
+    encrypt      = true
+    use_lockfile = true
+  }
 }
